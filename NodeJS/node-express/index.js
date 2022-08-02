3bad67f53bba9dfc48999fdcb28a5299782e4401
SharyDev <shahroz.abbas2018@gmail.com>
2022-08-02 18:08:15 +0500

    Express

--- NodeJS/node-express/index.js
+++ NodeJS/node-express/index.js
@@ -7,7 +7,7 @@ const port = 3000;
 
 const app = express();
 app.use(morgan("dev"));
-app.use(express.static(__dirname + "./public"));
+app.use(express.static(__dirname + "/public"));
 app.use((req,res,next) => {
     res.statusCode =200;
     console.log(req.header);
