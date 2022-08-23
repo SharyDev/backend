85a1c9e875d41823dcc3dd248e617a9e0d8f40c6
SharyDev <shahroz.abbas2018@gmail.com>
2022-08-24 03:41:03 +0500

    File Upload

--- NodeJS/conFusionServer/app.js
+++ NodeJS/conFusionServer/app.js
@@ -17,6 +17,7 @@ var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
+const uploadRouter = require('./routes/uploadRouter');
 
 const Dishes = require('./models/dishes');
 
@@ -63,7 +64,7 @@ connect.then((db) => {
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+app.use('/imageUpload',uploadRouter);
 app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
