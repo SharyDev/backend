3f7fcf7b90bd1541d87d27a6837df18dd16bb05a
SharyDev <shahroz.abbas2018@gmail.com>
2022-08-30 03:19:38 +0500

    Readme added

--- NodeJS/conFusionServer/app.js
+++ NodeJS/conFusionServer/app.js
@@ -18,6 +18,7 @@ var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 const uploadRouter = require('./routes/uploadRouter');
+var favRouter = require("./routes/favoriteRouter");
 
 const Dishes = require('./models/dishes');
 
@@ -68,6 +69,7 @@ app.use('/imageUpload',uploadRouter);
 app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
+app.use("/favorites",favRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
